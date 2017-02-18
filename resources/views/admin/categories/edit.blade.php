7d4d40e8fa06570180cfa3421b164560a3ea6d87
ThistleApps <mduncan77@comcast.net>
2017-02-18 07:44:56 -0500

    First Commit

---
+++ resources/views/admin/categories/edit.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Heading</div>
+
+                <div class="panel-body">
+                    Body copy to be edited.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
